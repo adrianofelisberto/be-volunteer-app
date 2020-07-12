185b45989d00716bdc6a6eaf91acef217b7ef6b9
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-07-12 17:35:59 -0300

    feat: create home component

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     PublicRoutingModule
